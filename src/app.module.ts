99af5c537355c22c0590ee68d7744a678a1ff354
abdellah <abdellahedaoudi80@gmail.com>
2024-11-25 00:06:45 +0100

    Adding more than one language in a practical way (i18n in Nestjs)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [I18nModule.forRoot({
+    fallbackLanguage: 'en',
+    loaderOptions: {
+      path: path.join(__dirname, '/i18n/'),
+      watch: true,
+    },
+    resolvers: [
+      { use: QueryResolver, options: ['lang'] },
+      AcceptLanguageResolver,
+      new HeaderResolver(['x-lang']),
+    ],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
